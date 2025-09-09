91339e84447b5d7e965538feb22c3554039e89be
Anurag Maiti <knoxxus.dev@gmail.com>
2025-09-09 16:58:30 +0530

    Migrate 10 older problems following naming convention

---
+++ Codeforces/1982C-BoringDay.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pii pair<int,int>
+#define ld long double
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define uii unordered_map<int,int>
+#define f(i,n) for(int i=0;i<n;i++)
+#define vi vector<int>
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+using namespace std;
+
+void sol(){
+    ll n,l,r;cin>>n>>l>>r;
+    vector<ll>a(n);
+    for(auto &it:a)cin>>it;
+    int it1=0,it2=0;
+    ll sum=0;
+    int ans=0;
+    while(it2<n){
+        sum+=a[it2];
+        if(sum>=l&&sum<=r){
+            ans++;
+            sum=0;
+            it2++;
+            it1=it2;
+        }else if(sum<l){
+            it2++;
+        }else{
+            bool flag=0;
+            while(it1<it2){
+                sum-=a[it1];
+                if(sum>=l&&sum<=r){
+                    ans++;break;
+                }
+                it1++;
+                if(sum<l){
+                    it2++;
+                    flag=1;break;
+                }
+            }
+            if(flag){
+                continue;
+            }
+            sum=0;
+            it2++;
+            it1=it2;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main(){
+    IOS;
+    int t;cin>>t;while(t--) sol();
+    return 0;
+}
