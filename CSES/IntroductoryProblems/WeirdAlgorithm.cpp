42f9c6b229b461b2523de89d05884742bf7c97fa
knoxxus-dev <knoxxus.dev@gmail.com>
2026-01-09 19:32:29 +0530

    Solve CSES Weird Algorithm

---
+++ CSES/IntroductoryProblems/WeirdAlgorithm.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define ld long double
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+const int MAX_N=1e5+5;
+const ll MOD=1e9+7;
+const ll INF=1e9;
+const ld EPS=1e-9;
+
+using namespace std;
+
+void sol() {
+    ll n; cin >> n;
+    while(n != 1) {
+        cout << n << " ";
+        if(n % 2) {
+            n = n * 3 + 1;
+        } else {
+            n = n / 2;
+        }
+    }
+    cout << n;
+}
+
+int main() {
+    IOS;
+    sol();
+    return 0;
+}
