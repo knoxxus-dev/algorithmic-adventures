4843e56c908878af38f35771e159e6a6cd99bd43
knoxxus-dev <knoxxus.dev@gmail.com>
2026-01-09 19:31:49 +0530

    Solve CSES Missing Number

---
+++ CSES/IntroductoryProblems/MissingNumber.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define ld long double
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+const int MAX_N=1e5+5;
+const ll MOD=1e9+7;
+const ll INF=1e9;
+const ld EPS=1e-9;
+
+using namespace std;
+
+void sol() {
+    ll n; cin >> n;
+    ll actualSum = (n * (n + 1)) / 2;
+    ll sum = 0;
+    for(int i = 0; i < n - 1; i++) {
+        ll x; cin >> x;
+        sum += x;
+    }
+    cout << actualSum - sum << endl;
+}
+
+int main() {
+    IOS;
+    sol();
+    return 0;
+}
