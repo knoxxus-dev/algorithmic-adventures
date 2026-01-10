2b56c1cff6fb4291a11f97e0dd270bee178c693b
knoxxus-dev <knoxxus.dev@gmail.com>
2026-01-10 12:58:16 +0530

    Solve CSES Distinct Numbers

---
+++ CSES/SortingAndSearching/DistinctNumbers.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define ld long double
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+const int MAX_N=1e5+5;
+const ll MOD=1e9+7;
+const ll INF=1e9;
+const ld EPS=1e-9;
+
+using namespace std;
+
+void sol() {
+    int n; cin >> n;
+    int ans = 1;
+    vector<int> arr(n);
+    for(int &it: arr) {
+        cin >> it;
+    }
+    sort(all(arr));
+    for(int i = 1; i < n; i++) {
+        if(arr[i - 1] != arr[i]) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    IOS;
+    sol();
+    return 0;
+}
