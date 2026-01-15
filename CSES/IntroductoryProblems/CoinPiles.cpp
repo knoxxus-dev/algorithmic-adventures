e75eb587b4463cf002cb478cf56e00b4c8ee19bd
knoxxus-dev <knoxxus.dev@gmail.com>
2026-01-15 12:25:47 +0530

    Solve CSES Coin Piles

---
+++ CSES/IntroductoryProblems/CoinPiles.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define ld long double
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+const int MAX_N=1e5+5;
+const ll MOD=1e9+7;
+const ll INF=1e9;
+const ld EPS=1e-9;
+
+using namespace std;
+
+void sol() {
+    ll a, b; cin >> a >> b;
+    if((a + b) % 3 == 0 && a <= 2 * b && b <= 2 * a) {
+        cout << "YES" << endl;
+        return;
+    }
+    cout << "NO" << endl;
+}
+
+int main() {
+    IOS;
+    int t;cin>>t;while(t--) sol();
+    return 0;
+}
