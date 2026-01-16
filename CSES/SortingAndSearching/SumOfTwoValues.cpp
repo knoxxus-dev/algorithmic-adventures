2897e19858205da197ea8e314d684082db0fb113
knoxxus-dev <knoxxus.dev@gmail.com>
2026-01-16 14:07:04 +0530

    Solve CSES Sum of Two Values

---
+++ CSES/SortingAndSearching/SumOfTwoValues.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define ld long double
+#define ll long long
+#define all(v) v.begin(), v.end()
+#define pb push_back
+
+int dx[4]={0,1,0,-1};
+int dy[4]={1,0,-1,0};
+
+const int MAX_N=1e5+5;
+const ll MOD=1e9+7;
+const ll INF=1e9;
+const ld EPS=1e-9;
+
+using namespace std;
+
+void sol() {
+    ll n, x; cin >> n >> x;
+    vector<ll> arr(n + 1);
+    for(ll i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    map<ll, ll> elements;
+    for(ll i = 1; i <= n; i++) {
+        if(elements.count(x - arr[i]) == 0) {
+            elements[arr[i]] = i;
+        } else {
+            cout << elements[x - arr[i]] << " " << i;
+            return;
+        }
+    }
+    cout << "IMPOSSIBLE";
+}
+
+int main() {
+    IOS;
+    sol();
+    return 0;
+}
